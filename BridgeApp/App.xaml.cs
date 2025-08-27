8a5170a06458803a76e1e9bb3c5caae25c45e2b5
Austin <austin_hammell@yahoo.com>
2025-08-27 17:19:54 -0500

    Renamed Solution to BridgeApp

---
+++ BridgeApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace BridgeApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ BridgeApp/obj/Debug/net7.0-android/android/src/mono/android/app/ApplicationRegistration.java
@@ -0,0 +1,12 @@
+package mono.android.app;
+
+public class ApplicationRegistration {
+
+	public static void registerApplications ()
+	{
+				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("BridgeApp.MainApplication, BridgeApp, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", crc64689b3fc5c9141e46.MainApplication.class, crc64689b3fc5c9141e46.MainApplication.__md_methods);
+		mono.android.Runtime.register ("Microsoft.Maui.MauiApplication, Microsoft.Maui, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", crc6488302ad6e9e4df1a.MauiApplication.class, crc6488302ad6e9e4df1a.MauiApplication.__md_methods);
+		
+	}
+}
